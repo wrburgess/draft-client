1715955633dd7b01837b445beadf7efdec823cee
Randy Burgess <wrburgess@wrburgess.com>
2013-08-26 19:07:45 -0500

    finishing fixtures, adding routes

--- js/models/slot.js
+++ js/models/slot.js
@@ -3,3 +3,440 @@ Slots.Draft = DS.Model.extend({
   name: DS.attr('string'),
   weight: DS.attr('integer')
 });
+
+
+Slots.Draft.FIXTURES = 
+[
+{
+"id": 1,
+"rosterId": 1,
+"name": "Start",
+"weight": 1
+},
+{
+"id": 2,
+"rosterId": 1,
+"name": "Start",
+"weight": 2
+},
+{
+"id": 3,
+"rosterId": 1,
+"name": "Start",
+"weight": 3
+},
+{
+"id": 4,
+"rosterId": 1,
+"name": "Start",
+"weight": 4
+},
+{
+"id": 5,
+"rosterId": 1,
+"name": "Start",
+"weight": 5
+},
+{
+"id": 6,
+"rosterId": 1,
+"name": "Start",
+"weight": 6
+},
+{
+"id": 1,
+"rosterId": 2,
+"name": "Start",
+"weight": 1
+},
+{
+"id": 2,
+"rosterId": 2,
+"name": "Start",
+"weight": 2
+},
+{
+"id": 3,
+"rosterId": 2,
+"name": "Start",
+"weight": 3
+},
+{
+"id": 4,
+"rosterId": 2,
+"name": "Start",
+"weight": 4
+},
+{
+"id": 5,
+"rosterId": 2,
+"name": "Start",
+"weight": 5
+},
+{
+"id": 6,
+"rosterId": 2,
+"name": "Start",
+"weight": 6
+},
+{
+"id": 1,
+"rosterId": 3,
+"name": "Start",
+"weight": 1
+},
+{
+"id": 2,
+"rosterId": 3,
+"name": "Start",
+"weight": 2
+},
+{
+"id": 3,
+"rosterId": 3,
+"name": "Start",
+"weight": 3
+},
+{
+"id": 4,
+"rosterId": 3,
+"name": "Start",
+"weight": 4
+},
+{
+"id": 5,
+"rosterId": 3,
+"name": "Start",
+"weight": 5
+},
+{
+"id": 6,
+"rosterId": 3,
+"name": "Start",
+"weight": 6
+},
+{
+"id": 1,
+"rosterId": 4,
+"name": "Start",
+"weight": 1
+},
+{
+"id": 2,
+"rosterId": 4,
+"name": "Start",
+"weight": 2
+},
+{
+"id": 3,
+"rosterId": 4,
+"name": "Start",
+"weight": 3
+},
+{
+"id": 4,
+"rosterId": 4,
+"name": "Start",
+"weight": 4
+},
+{
+"id": 5,
+"rosterId": 4,
+"name": "Start",
+"weight": 5
+},
+{
+"id": 6,
+"rosterId": 4,
+"name": "Start",
+"weight": 6
+},
+{
+"id": 1,
+"rosterId": 5,
+"name": "Start",
+"weight": 1
+},
+{
+"id": 2,
+"rosterId": 5,
+"name": "Start",
+"weight": 2
+},
+{
+"id": 3,
+"rosterId": 5,
+"name": "Start",
+"weight": 3
+},
+{
+"id": 4,
+"rosterId": 5,
+"name": "Start",
+"weight": 4
+},
+{
+"id": 5,
+"rosterId": 5,
+"name": "Start",
+"weight": 5
+},
+{
+"id": 6,
+"rosterId": 5,
+"name": "Start",
+"weight": 6
+},
+{
+"id": 1,
+"rosterId": 6,
+"name": "Start",
+"weight": 1
+},
+{
+"id": 2,
+"rosterId": 6,
+"name": "Start",
+"weight": 2
+},
+{
+"id": 3,
+"rosterId": 6,
+"name": "Start",
+"weight": 3
+},
+{
+"id": 4,
+"rosterId": 6,
+"name": "Start",
+"weight": 4
+},
+{
+"id": 5,
+"rosterId": 6,
+"name": "Start",
+"weight": 5
+},
+{
+"id": 6,
+"rosterId": 6,
+"name": "Start",
+"weight": 6
+},
+{
+"id": 1,
+"rosterId": 7,
+"name": "Start",
+"weight": 1
+},
+{
+"id": 2,
+"rosterId": 7,
+"name": "Start",
+"weight": 2
+},
+{
+"id": 3,
+"rosterId": 7,
+"name": "Start",
+"weight": 3
+},
+{
+"id": 4,
+"rosterId": 7,
+"name": "Start",
+"weight": 4
+},
+{
+"id": 5,
+"rosterId": 7,
+"name": "Start",
+"weight": 5
+},
+{
+"id": 6,
+"rosterId": 7,
+"name": "Start",
+"weight": 6
+},
+{
+"id": 1,
+"rosterId": 8,
+"name": "Start",
+"weight": 1
+},
+{
+"id": 2,
+"rosterId": 8,
+"name": "Start",
+"weight": 2
+},
+{
+"id": 3,
+"rosterId": 8,
+"name": "Start",
+"weight": 3
+},
+{
+"id": 4,
+"rosterId": 8,
+"name": "Start",
+"weight": 4
+},
+{
+"id": 5,
+"rosterId": 8,
+"name": "Start",
+"weight": 5
+},
+{
+"id": 6,
+"rosterId": 8,
+"name": "Start",
+"weight": 6
+},
+{
+"id": 1,
+"rosterId": 9,
+"name": "Start",
+"weight": 1
+},
+{
+"id": 2,
+"rosterId": 9,
+"name": "Start",
+"weight": 2
+},
+{
+"id": 3,
+"rosterId": 9,
+"name": "Start",
+"weight": 3
+},
+{
+"id": 4,
+"rosterId": 9,
+"name": "Start",
+"weight": 4
+},
+{
+"id": 5,
+"rosterId": 9,
+"name": "Start",
+"weight": 5
+},
+{
+"id": 6,
+"rosterId": 9,
+"name": "Start",
+"weight": 6
+},
+{
+"id": 1,
+"rosterId": 10,
+"name": "Start",
+"weight": 1
+},
+{
+"id": 2,
+"rosterId": 10,
+"name": "Start",
+"weight": 2
+},
+{
+"id": 3,
+"rosterId": 10,
+"name": "Start",
+"weight": 3
+},
+{
+"id": 4,
+"rosterId": 10,
+"name": "Start",
+"weight": 4
+},
+{
+"id": 5,
+"rosterId": 10,
+"name": "Start",
+"weight": 5
+},
+{
+"id": 6,
+"rosterId": 10,
+"name": "Start",
+"weight": 6
+},
+{
+"id": 1,
+"rosterId": 11,
+"name": "Start",
+"weight": 1
+},
+{
+"id": 2,
+"rosterId": 11,
+"name": "Start",
+"weight": 2
+},
+{
+"id": 3,
+"rosterId": 11,
+"name": "Start",
+"weight": 3
+},
+{
+"id": 4,
+"rosterId": 11,
+"name": "Start",
+"weight": 4
+},
+{
+"id": 5,
+"rosterId": 11,
+"name": "Start",
+"weight": 5
+},
+{
+"id": 6,
+"rosterId": 11,
+"name": "Start",
+"weight": 6
+},
+{
+"id": 1,
+"rosterId": 12,
+"name": "Start",
+"weight": 1
+},
+{
+"id": 2,
+"rosterId": 12,
+"name": "Start",
+"weight": 2
+},
+{
+"id": 3,
+"rosterId": 12,
+"name": "Start",
+"weight": 3
+},
+{
+"id": 4,
+"rosterId": 12,
+"name": "Start",
+"weight": 4
+},
+{
+"id": 5,
+"rosterId": 12,
+"name": "Start",
+"weight": 5
+},
+{
+"id": 6,
+"rosterId": 12,
+"name": "Start",
+"weight": 6
+}
+]
--- js/router.js
+++ js/router.js
@@ -1,3 +1,40 @@
 Draft.Router.map(function () {
   this.resource('draft', { path: '/' });
 });
+
+Drafts.DraftsRoute = Ember.Route.extend({
+  model: function () {
+    return Drafts.Draft.find();
+  }
+});
+
+Rosters.RostersRoute = Ember.Route.extend({
+  model: function () {
+    return Rosters.Draft.find();
+  }
+});
+
+Slots.SlotsRoute = Ember.Route.extend({
+  model: function () {
+    return Slots.Draft.find();
+  }
+});
+
+Picks.PicksRoute = Ember.Route.extend({
+  model: function () {
+    return Picks.Draft.find();
+  }
+});
+
+Pieces.PiecesRoute = Ember.Route.extend({
+  model: function () {
+    return Pieces.Draft.find();
+  }
+});
+
+Users.UsersRoute = Ember.Route.extend({
+  model: function () {
+    return Users.Draft.find();
+  }
+});
+
